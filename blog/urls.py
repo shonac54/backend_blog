8c41dbc34d2c6b08b94aab028ce705c27a763402
shonac54 <shonac369@gmail.com>
2023-11-17 21:14:56 +0530

    registered user

---
+++ blog/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path,include
+from . import views
+urlpatterns = [
+    path('adduser/',views.userRegister,name='adduser')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from blog.serializer import RegisterSerializer
+
+from django.db.models import Q
 
 # Create your views here.
+@csrf_exempt
+def userRegister(request):
+    if request.method == "POST":
+        recieved_data = json.loads(request.body)
+        print(recieved_data)
+        serialized_data = RegisterSerializer(data =recieved_data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return HttpResponse(json.dumps({"status":"success"}))
+        else:
+            return HttpResponse(json.dumps({"status":"failed"}))
+
